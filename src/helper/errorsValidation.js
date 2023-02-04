369f06707fd1b90d875dabe544e43838666e876d
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-02-03 23:46:23 -0300

    feat: consecutiveError

--- src/helper/errorsValidation.js
+++ src/helper/errorsValidation.js
@@ -1,10 +1,14 @@
 class OrderValidationError extends Error {
   typeError(keyCase, typeExpected) {
-    throw new Error(`Type error: Some file had "${keyCase}" different from ${typeExpected}`);
+    throw new Error(`Type error: Some file had "${keyCase}" different from ${typeExpected}.`);
   }
 
   uniqueValueError() {
-    throw new Error('The files have improper repetitions in the key "numero_item"');
+    throw new Error('The files have improper repetitions in the key "numero_item".');
+  }
+
+  consecutiveError() {
+    throw new Error('The "numero_item" table must be consecutive numbers, with no gaps between the largest and smallest.');
   }
 }
 

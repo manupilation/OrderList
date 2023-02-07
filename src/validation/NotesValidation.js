df627bda6ca898de166140a0ab6a797d990fafd0
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-02-07 00:12:32 -0300

    refactor: RegisterOrders

---
+++ src/validation/NotesValidation.js
@@ -0,0 +1,37 @@
+const NoteValidationError = require('../helper/errorsValidation');
+
+class NoteValidator {
+  constructor(note) {
+    this.note = note;
+    this.validationError = new NoteValidationError();
+    this.init();
+  }
+
+  noteValidator() {
+    this.note.data.map((note) => {
+      this.typesValidator(note);
+      return true;
+    });
+
+    console.log('----------------------------------------------------------------------');
+  }
+
+  typesValidator(note) {
+    if (typeof note.id_pedido !== 'number') {
+      this.validationError.typeError('id_pedido', 'string');
+    }
+    if (typeof note.numero_item !== 'number') {
+      this.validationError.typeError('numero_item', 'number');
+    }
+    if (typeof note.quantidade_produto !== 'number') {
+      this.validationError.typeError('quantidade_produto', 'number');
+    }
+  }
+
+  init() {
+    this.noteValidator();
+    return true;
+  }
+}
+
+module.exports = { NoteValidator };
